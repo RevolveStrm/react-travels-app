e5ce0402a3dcd2bba72019d780c9a8aae4a113df
RevolveStrm <revolvebattlerage@gmail.com>
2024-01-21 22:24:20 +0200

    Now site shows message if no trips were found with given filters.

--- src/components/Trips.tsx
+++ src/components/Trips.tsx
@@ -11,7 +11,10 @@ const Trips: React.FC<TripsProps> = ({ trips }) => {
         <section className="trips">
             <h2 className="visually-hidden">Trips List</h2>
             <ul className="trip-list">
-                {trips?.length && trips.map(el => <TripCard key={el.id} trip={el}/>)}
+                {   trips?.length ?
+                    trips.map(el => <TripCard key={el.id} trip={el}/>) :
+                    <h4>Couldn't find any trips. Please, check your filters.</h4>
+                }
             </ul>
         </section>
     );
